93b119fb39ff4f8f5bf987ecec2d8c3654c85c61
Charles Lee <charlesjhlee@gmail.com>
2020-12-23 20:04:30 +1100

    t

--- implement/applications/eve-online/eve-online-mining-bot/Common/EffectOnWindow.elm
+++ implement/applications/eve-online/eve-online-mining-bot/Common/EffectOnWindow.elm
@@ -41,6 +41,12 @@ effectsForDragAndDrop : { startLocation : Location2d, mouseButton : MouseButton,
 effectsForDragAndDrop { startLocation, mouseButton, endLocation } =
     [ MouseMoveTo startLocation
     , KeyDown (virtualKeyCodeFromMouseButton mouseButton)
+    , KeyUp (virtualKeyCodeFromMouseButton mouseButton)
+    , KeyDown (vkey_LCONTROL)
+    , KeyDown (vkey_A)
+    , KeyUp (vkey_LCONTROL)
+    , KeyUp (vkey_A)
+    , KeyDown (virtualKeyCodeFromMouseButton mouseButton)
     , MouseMoveTo endLocation
     , KeyUp (virtualKeyCodeFromMouseButton mouseButton)
     , KeyDown (virtualKeyCodeFromMouseButton mouseButton)
